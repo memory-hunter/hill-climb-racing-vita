791a6c47892ad06f020f6b1ed9883d3e6b88d912
Davit Petriashvili <59766692+memory-hunter@users.noreply.github.com>
2025-10-18 23:23:09 +0200

    Restore pricing settings

--- source/main.c
+++ source/main.c
@@ -87,6 +87,7 @@ int main()
     int (*Game_nativeInit)(void *env, void *obj, jint screen_width, jint screen_height) = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit");
     int (*Game_nativeResize)(void *env, void *obj, jint screen_width, jint screen_height) = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeResize");
     int (*Game_nativeRender)(void *env) = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeRender");
+    int (*Game_setInAppItemPrice)(void *env, void *obj, jstring item, jstring price) = (void *)so_symbol(&so_mod, "Java_com_fingersoft_game_MainActivity_setInAppItemPrice");
 
     Cocos2dx_nativeTouchesBegin = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesBegin");
     Cocos2dx_nativeTouchesMove = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesMove");
@@ -94,6 +95,36 @@ int main()
     Cocos2dx_nativeKeyDown = (void *)so_symbol(&so_mod, "Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeKeyDown");
 
     JNI_OnLoad(&jvm);
+
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_150000coins"), jni->NewStringUTF(&jni, "1.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_300000coins"), jni->NewStringUTF(&jni, "2.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_750000coins"), jni->NewStringUTF(&jni, "4.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_2000000coins"), jni->NewStringUTF(&jni, "9.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_4000000coins"), jni->NewStringUTF(&jni, "16.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_8000000coins"), jni->NewStringUTF(&jni, "25.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.adfree_20000000coins"), jni->NewStringUTF(&jni, "49.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_300000coins"), jni->NewStringUTF(&jni, "1.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_600000coins"), jni->NewStringUTF(&jni, "2.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_1500000coins"), jni->NewStringUTF(&jni, "4.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_4000000coins"), jni->NewStringUTF(&jni, "9.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_8000000coins"), jni->NewStringUTF(&jni, "16.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_16000000coins"), jni->NewStringUTF(&jni, "25.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap2.adfree_40000000coins"), jni->NewStringUTF(&jni, "49.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_600000coins"), jni->NewStringUTF(&jni, "1.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_1200000coins"), jni->NewStringUTF(&jni, "2.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_3000000coins"), jni->NewStringUTF(&jni, "4.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_8000000coins"), jni->NewStringUTF(&jni, "9.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_16000000coins"), jni->NewStringUTF(&jni, "16.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_32000000coins"), jni->NewStringUTF(&jni, "25.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap3.adfree_80000000coins"), jni->NewStringUTF(&jni, "49.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_1200000coins"), jni->NewStringUTF(&jni, "1.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_2400000coins"), jni->NewStringUTF(&jni, "2.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_6000000coins"), jni->NewStringUTF(&jni, "4.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_16000000coins"), jni->NewStringUTF(&jni, "9.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_32000000coins"), jni->NewStringUTF(&jni, "16.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_64000000coins"), jni->NewStringUTF(&jni, "25.99"));
+    Game_setInAppItemPrice(&jni, NULL, jni->NewStringUTF(&jni, "com.fingersoft.hillclimb.iap4.adfree_160000000coins"), jni->NewStringUTF(&jni, "49.99"));
+
     Game_isTestingMode();
     Cocos2dx_nativeSetPaths(&jni, NULL, jni->NewStringUTF(&jni, "ux0:/data/hcr/base.apk"));
     Game_nativeInit(&jni, NULL, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -110,27 +141,3 @@ int main()
 
     return sceKernelExitDeleteThread(0);
 }
-
-/*
-Java_com_fingersoft_game_MainActivity_onControllerKeyEvent
-Java_com_fingersoft_game_MainActivity_onControllerConnectionEvent
-Java_com_fingersoft_utils_CloudHelper_onSignInCompleted
-Java_com_fingersoft_utils_CloudHelper_onSyncDeviceLimitReached
-Java_com_fingersoft_utils_CloudHelper_onSyncDataLoaded
-Java_com_fingersoft_utils_CloudHelper_onAchievementProgressLoaded
-Java_org_cocos2dx_lib_Cocos2dxBitmap_nativeInitBitmapDC
-Java_org_cocos2dx_lib_Cocos2dxAccelerometer_onSensorChanged
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeResize
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInit
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeInsertText
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeDeleteBackward
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeGetContentText
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeRender
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnPause
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeOnResume
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesBegin
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesEnd
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesMove
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeTouchesCancel
-Java_org_cocos2dx_lib_Cocos2dxRenderer_nativeKeyDown
-*/
